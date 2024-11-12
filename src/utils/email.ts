62a4ba68f564e624b6e5519e22e6165ddb3cc783
huxw <huxw@mail.jj.cn>
2024-11-12 20:48:22 +0800

    删除react相关

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,7 +1,5 @@
 import process from 'node:process';
 import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
-import { render } from '@react-email/render';
-import { VerificationEmail } from '../templates/verification-email';
 
 let client: SESClient;
 
@@ -17,40 +15,4 @@ export function getEmailClient() {
   }
 
   return client;
-}
-
-export async function sendVerificationEmail(baseUrl: string, name: string, email: string, code: string) {
-  try {
-    const client = getEmailClient();
-    const { FROM_NAME, FROM_EMAIL } = process.env;
-
-    const emailHtml = render(VerificationEmail({ baseUrl, name, email, code }));
-
-    const params = {
-      Source: `${FROM_NAME} <${FROM_EMAIL}>`,
-      Destination: {
-        ToAddresses: [email],
-      },
-      Message: {
-        Subject: {
-          Charset: 'UTF-8',
-          Data: 'Verify your email!',
-        },
-        Body: {
-          Html: {
-            Charset: 'UTF-8',
-            Data: emailHtml,
-          },
-        },
-      },
-    };
-
-    const command = new SendEmailCommand(params);
-
-    const res = await client.send(command);
-    return res.$metadata.httpStatusCode;
-  }
-  catch (_err: any) {
-    return 500;
-  }
 }
