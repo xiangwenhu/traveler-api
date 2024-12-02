333fd669c4a2d3fdc2acd6356fc51a07d7653a24
huxw <huxw@mail.jj.cn>
2024-12-02 17:21:52 +0800

    新增map的代理

--- src/routes/proxy.ts
+++ src/routes/proxy.ts
@@ -42,4 +42,47 @@ export default function useProxy(app: express.Express) {
     }),
 
   }));
+
+
+
+  app.use('/api/proxy/map',
+    // authenticate({
+    //   verifyAdmin: false
+    // }),
+    createProxyMiddleware({
+      target: 'https://restapi.amap.com',
+      changeOrigin: true,
+      // selfHandleResponse: true,
+      // headers: {
+      //   "host": "localhost:3000",
+      //   origin: "http://localhost:3001",
+      //   referer: "http://localhost:3001/"
+      // },
+      pathRewrite(path) {
+        console.info('path:', path);
+        const targetPath = path.replace('/api/proxy/map', '') + `&key=${process.env.AMAP_KEY}`;
+        console.info('targetPath:', targetPath);
+        return targetPath;
+      },
+      // onProxyRes: responseInterceptor(async (responseBuffer, _proxyRes, _req, _res) => {
+      //   try {
+      //     const resText = responseBuffer.toString('utf8');
+      //     console.log("resText:", resText);
+      //     const data = JSON.parse(resText);
+      //     return JSON.stringify({
+      //       code: 0,
+      //       data,
+      //     });
+      //   }
+      //   catch (err: any) {
+      //     consola.error('onProxyRes error:', err);
+      //     return JSON.stringify({
+      //       code: EnumErrorCode.PROXY_ERROR,
+      //       message: err & err.message,
+      //     });
+      //   }
+      // }),
+
+    }));
+
 }
