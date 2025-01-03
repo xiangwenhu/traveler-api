0b5b4c9cbf2c384b8d9f0a5bfd4b93727f3f8d21
huxw <huxw@mail.jj.cn>
2025-01-03 21:01:03 +0800

    ice proxy

--- src/routes/ice.ts
+++ src/routes/ice.ts
@@ -2,15 +2,35 @@ import type { Router } from 'express';
 import { authenticate } from '../middlewares/auth';
 import { createRouter } from '../utils/create';
 import { submitMediaProducingHandler, submitTravelMediaProducingHandler } from '../controllers/ice';
+import iceClientProxy from '../lib/ICEClientProxy';
 
 export default createRouter((router: Router) => {
-  router.use(
-    authenticate({
-      verifyAdmin: false,
-    }),
-  );
+  // router.use(
+  //   authenticate({
+  //     verifyAdmin: false,
+  //   }),
+  // );
 
   router.post('/submitMediaProducing', submitMediaProducingHandler);
   
   router.post('/submitTravelMediaProducing', submitTravelMediaProducingHandler);
+
+  router.get("/proxy/get", async (req, res)=> {
+    const { Action, ...params } = req.query;
+    const data = await iceClientProxy.requestICEGet(Action as string, params);
+    return res.json({
+      code: 0,
+      data
+    })
+  })
+
+  router.get("/proxy/post", async (req, res)=> {
+    const { Action, ...params } = req.body;
+    const data = await iceClientProxy.requestICEPost(Action, params);
+    return res.json({
+      code: 0,
+      data
+    })
+  })
+
 });
