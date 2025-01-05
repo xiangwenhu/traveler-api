f2171b5065c4ae0787072e3f1d084d7c9e2fd655
huxw <huxw@mail.jj.cn>
2025-01-06 02:02:45 +0800

    1. ice接口调整

--- src/routes/ice.ts
+++ src/routes/ice.ts
@@ -12,25 +12,42 @@ export default createRouter((router: Router) => {
   // );
 
   router.post('/submitMediaProducing', submitMediaProducingHandler);
-  
+
   router.post('/submitTravelMediaProducing', submitTravelMediaProducingHandler);
 
-  router.get("/proxy/get", async (req, res)=> {
-    const { Action, ...params } = req.query;
-    const data = await iceClientProxy.requestICEGet(Action as string, params);
-    return res.json({
-      code: 0,
-      data
-    })
+  router.get("/proxy/get", async (req, res) => {
+
+    try {
+      const { Action, ...params } = req.query;
+      const data = await iceClientProxy.requestICEGet(Action as string, params);
+      return res.json({
+        code: 0,
+        data
+      })
+    } catch (err: any) {
+      return res.json({
+        code: 40010,
+        message: err.message ||  err.data.message,
+        data: err.data
+      })
+    }
   })
 
-  router.get("/proxy/post", async (req, res)=> {
-    const { Action, ...params } = req.body;
-    const data = await iceClientProxy.requestICEPost(Action, params);
-    return res.json({
-      code: 0,
-      data
-    })
+  router.post("/proxy/post", async (req, res) => {
+    try {
+      const { Action, ...params } = req.body;
+      const data = await iceClientProxy.requestICEPost(Action, params);
+      return res.json({
+        code: 0,
+        data
+      })
+    } catch (err: any) {
+      return res.json({
+        code: 40010,
+        message: err.message ||  err.data.message,
+        data: err.data
+      })
+    }
   })
 
 });
